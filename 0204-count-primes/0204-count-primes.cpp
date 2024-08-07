1fe7f42905852b08b15fc48bef6cf602d1663981
Khem Chand <124715350+Khemchand7@users.noreply.github.com>
2024-08-07 21:04:28 +0530

    Time: 427 ms (51.77%), Space: 11.7 MB (56.41%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 //Sieve of Eratosthemes Algorithm
-    int countPrimes(int n) {
+/*     int UnoptimizedcountPrimes(int n) {
         if (n == 0)
             return 0;
         vector<bool> prime(n, true);//n size ki vector create ki aur sabko prime mark kar dia 
@@ -17,6 +17,31 @@ public:
                 }
             }
         }
+        return ans;
+    } */
+
+    // Optimized Sieve of Eratosthenes Algorithm
+    int countPrimes(int n) {
+        if (n <= 2) // Since there are no prime numbers less than 2 as the que says it should be 
+                    //strictly less the n;
+            return 0;
+        
+        vector<bool> prime(n, true); // Initialize vector with 'true' to mark all numbers as prime
+        prime[0] = prime[1] = false; // 0 and 1 are not prime numbers
+        int ans = n - 2; // Start with all numbers marked as prime except 0 and 1 aisa islie lia kyuki loop me sirf jab hi enter kar rhe 
+                         // jab multiles ko hata rhe hai to ++ans se correct result nhi aaega this logic will work better;
+        
+        for (int i = 2; i * i < n; i++) { // Optimization: i * i < n
+            if (prime[i]) {
+                for (int j = i * i; j < n; j += i) { // Mark multiples of i as non-prime
+                    if (prime[j]) { // Only decrement count for the first time we mark it non-prime
+                        prime[j] = false;
+                        --ans;//yahan pr multiples ko hata rhe hai ans me se 
+                    }
+                }
+            }
+        }
+        
         return ans;
     }
 };
