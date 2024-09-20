a79b278e0092bb18e2ca3ae120c20036de802239
Khem Chand <124715350+Khemchand7@users.noreply.github.com>
2024-09-20 16:27:38 +0530

    Time: 0 ms (100%), Space: 9.3 MB (19.4%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
@@ -20,12 +20,27 @@ public:
         return len;
     }
     ListNode* middleNode(ListNode* head) {
+/*         //in this approach we are traversing the linked list twice we need the approach in which we get ans in one traversal
         int position = (getLength(head)/2) + 1;
         ListNode* temp = head;
         while(position!=1){
             temp = temp->next;
             position--;
         }
-        return temp;
+        return temp; */
+
+        //Slow and Fast Pointer Approach or Two Pointer algorithm or Tortoise Algorithm
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast!=NULL){
+            fast = fast-> next;//ek bar me 2 step islie nhi badae qki possibility hai ki fast NULL pr ho aise me wo null ke next pr chala jaega
+            if(fast!=NULL){
+                fast = fast -> next;
+                //yahan pr fast 2 step chal chuka hai to ab hum slow ko ek step bada sakte hai 
+                slow = slow -> next;
+            }
+        }
+        return slow;
     }
 };
