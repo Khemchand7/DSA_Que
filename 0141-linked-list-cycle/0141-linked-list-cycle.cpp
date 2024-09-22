66cd6a648a19bd5f361b78c4baa7c835cafe0b01
Khem Chand <124715350+Khemchand7@users.noreply.github.com>
2024-09-22 13:55:31 +0530

    Time: 12 ms (34.88%), Space: 10.6 MB (99.85%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -8,7 +8,8 @@
  */
 class Solution {
 public:
-    bool hasCycle(ListNode *head) {
+    //using map function
+    bool hasCycleUsingMap(ListNode *head) {
         map<ListNode* , bool> table;
         ListNode* temp = head;
         while(temp!=NULL){
@@ -20,6 +21,25 @@ public:
             }
             temp = temp->next;
         }
+        return false;
+    }
+    // slow and fast pointer || tortoise and hare algorithm || two pointer 
+    bool hasCycle(ListNode *head) {
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        while(fast!=NULL){
+            fast = fast->next;
+            if(fast!=NULL){
+                fast = fast->next;
+                slow = slow->next;
+            }
+            //if both the pointer meet then cycle present
+            if(fast == slow){
+                return true;
+            }
+        }
+        //loop se bahar aae matlab fast == NULL => cycle is not present
         return false;
     }
 };
