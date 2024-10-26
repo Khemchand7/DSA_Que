34fb5b1bc2d96fa8cd521bd83e21ad1dde071f3a
Khem Chand <124715350+Khemchand7@users.noreply.github.com>
2024-10-26 19:33:53 +0530

    Time: 142 ms (53.77%), Space: 126.4 MB (98.1%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        queue<int>qp;
-        queue<int>qn;
-        int n = nums.size();
-        for(int i=0;i<n;i++){
-            if(nums[i]>0) qp.push(nums[i]);
-            if(nums[i]<0) qn.push(nums[i]);
-        }
-        vector<int>ans;
-        for(int i=0;i<n/2;i++){
-            if(qp.empty()||qn.empty()) break;
-            int pos = qp.front();
-            qp.pop();
-            int neg = qn.front();
-            qn.pop();
-            ans.push_back(pos);
-            ans.push_back(neg);
-        }
-        return ans;
+        int n = nums.size(); // Get the size of the input array
+        int posIndex = 0;     // Index to place positive numbers
+        int negIndex = 1;     // Index to place negative numbers
+        vector<int> ans(n, 0); // Result vector initialized with zeros of size n
 
+        // Iterate over each element in the input array
+        for(int i = 0; i < n; i++) {
+            if(nums[i] > 0) {
+                ans[posIndex] = nums[i]; // Place positive number at posIndex
+                posIndex += 2;           // Move to the next position for positive numbers
+            }
+            if(nums[i] < 0) {
+                ans[negIndex] = nums[i]; // Place negative number at negIndex
+                negIndex += 2;           // Move to the next position for negative numbers
+            }
+        }
+
+        return ans; // Return the rearranged array
     }
 };
