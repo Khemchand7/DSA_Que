7908dc8757379810941712033d02983828f95248
Khem Chand <124715350+Khemchand7@users.noreply.github.com>
2026-02-07 21:50:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Minimum in BST/minimum-in-bst.cpp
@@ -0,0 +1,26 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int x) {
+        data = x;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int minValue(Node* root) {
+        if(!root) return 0;
+        
+        while(root->left != NULL) root = root->left;
+        
+        return root->data;
+        
+    }
+};
