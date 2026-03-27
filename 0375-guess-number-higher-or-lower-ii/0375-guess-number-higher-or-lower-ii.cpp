1ce6cbd199c5ce2bc20161ebf3b441bdfe888f96
Khem Chand <124715350+Khemchand7@users.noreply.github.com>
2026-03-27 23:55:00 +0530

    Time: 83 ms (30.85%), Space: 11.2 MB (24.58%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -23,15 +23,34 @@ public:
             ans = min(ans, i + max(solveMem(start, i - 1, dp),
                                    solveMem(i + 1, end, dp)));
         }
-        
+
         dp[start][end] = ans;
-        return ans;
+        return dp[start][end];
+    }
+
+    int solveTab(int n) {
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+        for (int start = n; start >= 1; start--) {
+            for (int end = 1; end <= n; end++) {
+                if(start >= end) continue;
+                int ans = INT_MAX;
+                for (int i = start; i <= end; i++) {
+                    ans = min(ans, i + max(solveMem(start, i - 1, dp),
+                                           solveMem(i + 1, end, dp)));
+                }
+
+                dp[start][end] = ans;
+            }
+        }
+        return dp[1][n];
     }
 
     int getMoneyAmount(int n) {
         // int ans = solveRec(1, n);
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
-        int ans = solveMem(1, n, dp);
+        /* vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        int ans = solveMem(1, n, dp); */
+        int ans = solveTab(n); 
 
         return ans;
     }
