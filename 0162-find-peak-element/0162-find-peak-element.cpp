67a829007d5f40c8debc87004399ec0f05e0e71b
Khem Chand <124715350+Khemchand7@users.noreply.github.com>
2026-07-07 15:29:50 +0530

    Time: 0 ms (100%), Space: 12.7 MB (5.8%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -2,21 +2,39 @@ class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
         int n = nums.size();
-        int s = 0;
-        int e = n - 1;
 
-        while (s < e) {
-            int mid = s + (e - s) / 2;
+        // If only one element is present, it is the peak.
+        if(n == 1) return 0;
 
-            // if left side is increasing, peak lies on right
-            if (nums[mid] < nums[mid + 1]) {
-                s = mid + 1;
-            } 
-            // otherwise, peak lies on left or at mid
-            else {
-                e = mid;
+        // Check if the first element is the peak.
+        if(nums[0] > nums[1]) return 0;
+
+        // Check if the last element is the peak.
+        if(nums[n - 1] > nums[n - 2]) return n - 1;
+
+        // Ignore the first and last elements as they are already checked.
+        int start = 1;
+        int end = n - 2;
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            // Peak element found.
+            if(nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]){
+                return mid;
+            }
+
+            // We are on the increasing slope, so peak lies on the right.
+            else if(nums[mid] > nums[mid - 1]){
+                start = mid + 1;
+            }
+
+            // We are on the decreasing slope, so peak lies on the left.
+            else{
+                end = mid - 1;
             }
         }
-        return s;  // index of peak element
+
+        return -1;
     }
 };
