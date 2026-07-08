f5eeff65c7cb26d7b7642b9deb97a93c99fd3209
Khem Chand <124715350+Khemchand7@users.noreply.github.com>
2026-07-08 20:57:38 +0530

    Time: 26 ms (33.67%), Space: 70.5 MB (47.31%) - LeetHub

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int numberOfBouquet(vector<int>& bloomDay, int totalBouquets, int flowersPerBouquet, int currentDay, int size){
+        int bouquetCount = 0;
+        int consecutiveFlowers = 0;
+
+        for(int i = 0; i < size; i++){
+            if(bloomDay[i] <= currentDay){
+                consecutiveFlowers++;
+
+                // Form a bouquet whenever k adjacent flowers are available
+                if(consecutiveFlowers == flowersPerBouquet){
+                    bouquetCount++;
+                    consecutiveFlowers = 0;
+                }
+            }
+            else{
+                // Reset count if an unbloomed flower breaks the sequence
+                consecutiveFlowers = 0;
+            }
+        }
+        return bouquetCount;
+    }
+
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        int n = bloomDay.size();
+
+        // Search space: minimum bloom day to maximum bloom day
+        int start = *min_element(bloomDay.begin(), bloomDay.end());
+        int end = *max_element(bloomDay.begin(), bloomDay.end());
+
+        int answer = -1;
+
+        while(start <= end){
+            // Check if it's possible to make bouquets on the current day
+            int mid = start + (end - start) / 2;
+
+            int bouquetsFormed = numberOfBouquet(bloomDay, m, k, mid, n);
+
+            if(bouquetsFormed >= m){
+                // Current day works, try to find an earlier valid day
+                answer = mid;
+                end = mid - 1;
+            }
+            else{
+                // Not enough bouquets, need to wait for more flowers to bloom
+                start = mid + 1;
+            }
+        }
+
+        return answer;
+    }
+};
