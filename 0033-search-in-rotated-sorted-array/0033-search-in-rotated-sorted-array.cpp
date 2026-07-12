5441fa1aa75e4ae8e7a874a8545cfffe76cb972d
Khem Chand <124715350+Khemchand7@users.noreply.github.com>
2026-07-12 23:51:30 +0530

    Time: 0 ms (100%), Space: 15.3 MB (34.82%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -5,42 +5,28 @@ public:
         int start = 0;
         int end = n - 1;
 
-        while (start <= end) {
-            // Calculate middle index safely
-            int mid = start + (end - start) / 2;
+        while(start <= end){
+            int mid = start + (end - start)/2;
 
-            // Target found
-            if (nums[mid] == target)
+            if(nums[mid] == target)
                 return mid;
-
-            // Check if the left half is sorted
-            else if (nums[start] <= nums[mid]) {
-
-                // Target lies within the sorted left half
-                if (target >= nums[start] && target <= nums[mid]) {
+            // if left half is sorted
+            else if(nums[start] <= nums[mid]){
+                if(target >= nums[start] && target <= nums[mid]){
                     end = mid - 1;
                 }
-                // Otherwise, search in the right half
-                else {
+                else{
                     start = mid + 1;
                 }
-            }
-
-            // Otherwise, the right half must be sorted
-            else {
-
-                // Target lies within the sorted right half
-                if (target >= nums[mid] && target <= nums[end]) {
-                    start = mid + 1;
+            }else{
+                if(target >= nums[mid] && target <= nums[end]){
+                     start = mid + 1;
                 }
-                // Otherwise, search in the left half
-                else {
+                else{
                     end = mid - 1;
                 }
             }
         }
-
-        // Target not present
         return -1;
     }
 };
