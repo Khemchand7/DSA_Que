a921023c7baf7d5fdba8a2627013217d093998a4
Khem Chand <124715350+Khemchand7@users.noreply.github.com>
2026-07-28 18:03:57 +0530

    Time: 12 ms (41.07%), Space: 38.7 MB (29.86%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,20 +1,43 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        int l = 0;
-        int h = arr.size()-1;
-        while(h-l>=k){
-            if(x-arr[l]>arr[h]-x){
-                l++;
+        int n = arr.size();
+
+        // Max Heap -> {distance, value}
+        priority_queue<pair<int, int>> pq;
+
+        for (int i = 0; i < n; i++) {
+
+            // Insert first k elements
+            if (pq.size() < k) {
+                pq.push({abs(arr[i] - x), arr[i]});
             }
-            else h--;
+
+            // If current element is closer, replace the farthest element
+            else if (pq.top().first > abs(arr[i] - x)) {
+                pq.pop();
+                pq.push({abs(arr[i] - x), arr[i]});
+            }
+
+            // If distance is same, prefer the smaller value
+            else if (pq.top().first == abs(arr[i] - x) &&
+                     pq.top().second > arr[i]) {
+                pq.pop();
+                pq.push({abs(arr[i] - x), arr[i]});
+            }
         }
-        //return vector<int>(arr.begin()+l,arr.begin()+h+1);
 
         vector<int> ans;
-        for(int i=l;i<=h;i++){
-            ans.push_back(arr[i]);
+
+        // Extract the k closest elements
+        while (!pq.empty()) {
+            ans.push_back(pq.top().second);
+            pq.pop();
         }
+
+        // Return the result in ascending order
+        sort(ans.begin(), ans.end());
+
         return ans;
     }
 };
