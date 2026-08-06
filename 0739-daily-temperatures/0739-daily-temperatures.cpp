500c4b699a11c203310d116e0ba8857c530d31d0
Khem Chand <124715350+Khemchand7@users.noreply.github.com>
2026-08-06 18:13:32 +0530

    Time: 13 ms (89.49%), Space: 103 MB (62.28%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,30 +1,31 @@
-        class Solution {
+class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         int n = temperatures.size();
-        vector<int> ans(n, 0);   // Default answer is 0 (no warmer day exists)
+        vector<int> ans(n, 0);   // Default is 0 (no warmer day exists)
 
-        // {temperature, index}
-        stack<pair<int, int>> st;
+        // Stores indices of temperatures in monotonic decreasing order
+        stack<int> st;
 
         // Traverse from right to left
         for (int i = n - 1; i >= 0; i--) {
 
-            // Remove all temperatures that are smaller than or equal to
-            // the current temperature since they can never be the next warmer day
-            while (!st.empty() && st.top().first <= temperatures[i]) {
+            // Remove all indices whose temperatures are
+            // less than or equal to the current temperature
+            while (!st.empty() && temperatures[st.top()] <= temperatures[i]) {
                 st.pop();
             }
 
-            // If a warmer day exists, store the number of days to wait
-            if (!st.empty())
-                ans[i] = st.top().second - i;
+            // If a warmer temperature exists, store the number of days to wait
+            if (!st.empty()) {
+                ans[i] = st.top() - i;
+            }
 
-            // Current temperature can be the next warmer day
+            // Push the current index as it may be the next warmer day
             // for temperatures on its left
-            st.push({temperatures[i], i});
+            st.push(i);
         }
 
         return ans;
     }
-};      
+};
